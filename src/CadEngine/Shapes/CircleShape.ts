7f3f16a94219bfaf9385a1c6e407b4bdcb2a5ded
Tarik EL JABIRI <tarik.eljabiri@gmail.com>
2022-09-05 18:14:03 +0200

    update circleshape

--- src/CadEngine/Shapes/CircleShape.ts
+++ src/CadEngine/Shapes/CircleShape.ts
@@ -1,6 +1,6 @@
 import { Shape } from '../Interfaces/Shape';
 import { ZoomManager } from '../ZoomManager';
-import { Arc, Circle, intersections, Line, Point } from '@mathigon/euclid';
+import { Arc, Circle, Line, Point } from '@mathigon/euclid';
 
 export class CircleShape implements Shape {
     center: Point;
@@ -15,8 +15,32 @@ export class CircleShape implements Shape {
     }
 
     intersects(geometry: Line | Arc | Circle) {
-        const c = new Circle(this.center, this.radius);
-        return intersections(geometry, c);
+        if(geometry instanceof Line) {
+            const dx = geometry.p2.x - geometry.p1.x
+            const dy = geometry.p2.y - geometry.p1.y
+            const distance = Point.distance(geometry.p1, geometry.p2)
+            const det = geometry.p1.x*geometry.p2.y - geometry.p2.x*geometry.p1.y
+            const delta = Math.pow(this.radius, 2) * Math.pow(distance, 2) - Math.pow(det, 2)
+            if (delta === 0) {
+                const x = det*dy/Math.pow(distance, 2)
+                const y = -det*dx/Math.pow(distance, 2)
+                const p = new Point(x, y)
+                return[p]
+            } else  {
+                let sgn = -1
+                if(dy > 0) sgn = 1
+                const x1 = (det*dy - sgn*dx*Math.sqrt(delta))/Math.pow(distance, 2)
+                const y1 = (-det*dx - Math.abs(dy) * Math.sqrt(delta))/Math.pow(distance, 2)
+
+                const x2 = (det*dy + sgn*dx*Math.sqrt(delta))/Math.pow(distance, 2)
+                const y2 = (-det*dx + Math.abs(dy) * Math.sqrt(delta))/Math.pow(distance, 2)
+
+                const p1 = new Point(x1, y1)
+                const p2 = new Point(x2, y2)
+                return[p1, p2]
+            }
+        }
+        return []
     }
 
     render(ctx: CanvasRenderingContext2D): void {
